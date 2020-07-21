be4c038259ac296f76a5894d7f4a8a4d75c40a9f
Artyom Sokol <skaliushartem@gmail.com>
2020-07-21 14:24:41 +0300

    Примеры работ. Сверстал галерею-плитку

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-$('[data-scroll*="#"]').click(function() {
-     $('html, body').animate({
-         scrollTop: $(this.dataset.scroll).offset().top
-     }, 600);
-     return false;
- });
+$(document).ready(function() {
+	$('[data-scroll*="#"]').click(function() {
+		let $goalElem = $(this.dataset.scroll);
+		if ($goalElem) {
+			$('html, body').animate({
+				scrollTop: $goalElem.offset().top
+			}, 600);
+		}
+		return false;
+	});
+	$('.lightzoom').lightzoom({
+		overlayOpacity: '0.7', // прозрачность фона
+		isOverlayClickClosing: true, // true, если надо закрывать окно при клике по любой области
+		isEscClosing: true, 
+	});
+});
